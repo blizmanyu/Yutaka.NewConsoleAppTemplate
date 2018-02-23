0a863b0791cc37b00e13b29ec7c40152cd4753dd
blizmanyu <blizman2017@gmail.com>
2018-02-23 12:39:44 -0800

    Add project files.

---
+++ Yutaka.NewConsoleAppTemplate/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using NLog;
+
+namespace Yutaka.NewConsoleAppTemplate
+{
+	class Program
+	{
+		#region Fields
+		#region Static Externs
+		[DllImport("kernel32.dll")]
+		static extern IntPtr GetConsoleWindow();
+		[DllImport("user32.dll")]
+		static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+		const int SW_HIDE = 0;
+		#endregion
+
+		// Constants //
+		const string PROGRAM_NAME = "NewConsoleAppTemplate";
+		const string TIMESTAMP = @"[HH:mm:ss] ";
+
+		// PIVs //
+		private static DateTime startTime = DateTime.Now;
+		private static Logger logger = LogManager.GetCurrentClassLogger();
+		private static bool consoleOut = true; // default = false //
+		private static int errorCount = 0;
+		private static int totalCount = 0;
+		private static int errorCountThreshold = 7;
+		private static double errorPerThreshold = 0.07;
+		#endregion
+
+		#region Private Helpers
+		#endregion
+
+		#region Methods
+		static void Process()
+		{
+
+		}
+
+		static void Main(string[] args)
+		{
+			StartProgram();
+			Process();
+			EndProgram();
+		}
+
+		static void StartProgram()
+		{
+			var log = String.Format("Starting {0} program", PROGRAM_NAME);
+			logger.Info(log);
+
+			if (consoleOut) {
+				Console.Clear();
+				Console.Write("{0}{1}", DateTime.Now.ToString(TIMESTAMP), log);
+			}
+
+			else {
+				var handle = GetConsoleWindow();
+				ShowWindow(handle, SW_HIDE); // hide window //
+			}
+		}
+
+		static void EndProgram()
+		{
+			var endTime = DateTime.Now;
+			var ts = endTime - startTime;
+			var errorPer = (double) errorCount/totalCount;
+
+			if (errorCount > errorCountThreshold && errorPer > errorPerThreshold)
+				logger.Error("The number of errors is above the threshold.");
+
+			var log = new string[4];
+			log[0] = "Ending program";
+			log[1] = String.Format("It took {0} to complete", ts.ToString(@"hh\:mm\:ss\.fff"));
+			log[2] = String.Format("Total: {0}", totalCount);
+			log[3] = String.Format("Errors: {0} ({1}){2}", errorCount, errorPer.ToString("P"), Environment.NewLine + Environment.NewLine);
+
+			logger.Info(log[0]);
+			logger.Info(log[1]);
+			logger.Info(log[2]);
+			logger.Info(log[3]);
+
+			if (consoleOut) {
+				Console.Write("\n");
+				Console.Write("\n{0}{1}", DateTime.Now.ToString(TIMESTAMP), log[0]);
+				Console.Write("\n{0}{1}", DateTime.Now.ToString(TIMESTAMP), log[1]);
+				Console.Write("\n{0}{1}", DateTime.Now.ToString(TIMESTAMP), log[2]);
+				Console.Write("\n{0}{1}", DateTime.Now.ToString(TIMESTAMP), log[3]);
+				Console.Write("\n.... Press any key to close the program ....");
+				Console.ReadKey(true);
+			}
+
+			Environment.Exit(0); // in case you want to call this method outside of a standard successful program completion, this line will close the app //
+		}
+		#endregion
+	}
+}
